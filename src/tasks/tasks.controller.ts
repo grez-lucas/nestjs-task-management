99718d8d5593c70f289f2407a4deba31ddd4883b
grez-lucas <lucasgrez3010@gmail.com>
2023-12-29 14:16:45 -0300

    Add Task model and implement getTasks method in TasksController and TasksService

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // Notice that we're using dependency injection to inject the TasksService into the TasksController.
+  // If we make this private tasksService: TasksService, then we can use this.tasksService in our methods.
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(): Task[] {
+    return this.tasksService.getTasks();
+  }
+}
