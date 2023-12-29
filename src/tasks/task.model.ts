e8f34ffce46d69c3bd3db5ca3a89b6e40d050e4c
grez-lucas <lucasgrez3010@gmail.com>
2023-12-29 14:30:49 -0300

    Add createTask method to TasksController

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -11,7 +11,7 @@ export interface Task {
   status: TaskStatus;
 }
 
-enum TaskStatus {
+export enum TaskStatus {
   OPEN = 'OPEN',
   IN_PROGRESS = 'IN_PROGRESS',
   DONE = 'DONE',
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-  // Notice that we're using dependency injection to inject the TasksService into the TasksController.
-  // If we make this private tasksService: TasksService, then we can use this.tasksService in our methods.
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getTasks(): Task[] {
     return this.tasksService.getTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log('Body: ', title, description);
+    return this.tasksService.createTask(title, description);
   }
 }
